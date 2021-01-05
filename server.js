5012715e4a2057f9df6a95b6c8c3575163eb74a2
Take-Some-Bytes <57299445+Take-Some-Bytes@users.noreply.github.com>
2021-01-04 20:50:20 -0800

    Added and updated middlewares

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
 const fs = require('fs')
 const path = require('path')
 const http = require('http')
+const qs = require('querystring')
 const connect = require('connect')
 const serveStatic = require('serve-static')
 const finalHandler = require('finalhandler')
@@ -17,6 +18,18 @@ let port = process.argv[2] || 5555
 
 app.use((req, res, next) => {
   console.log(`Request received for URL ${req.url} with method ${req.method}.`)
+  next()
+})
+app.use((req, res, next) => {
+  const url = new URL(req.url, `http://localhost:${port}`)
+  const query = url.search
+  // Parse the query string.
+  if (query && /^\?.+/.test(query)) {
+    const parsedQs = qs.parse(url.search.substring(1))
+    if (parsedQs && typeof parsedQs === 'object') {
+      req.query = parsedQs
+    }
+  }
   next()
 })
 app.use(serveStatic(path.join(__dirname, 'client'), {
@@ -37,11 +50,47 @@ app.use('/favicon.ico', (req, res, next) => {
   stream.on('error', err => {
     console.log(err.stack)
     stream.destroy()
-    return next()
+    return next(err)
   })
+})
+app.use('/xhr', (req, res, next) => {
+  const query = req.query
+
+  if (!query || typeof query !== 'object') {
+    res.statusCode = 400
+    next(new Error('Query is required!'))
+    return
+  } else if (!query.for) {
+    res.statusCode = 400
+    next(new Error('For parameter is required in query!'))
+    return
+  }
+
+  switch (query.for) {
+    case 'serversAvailable': {
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'application/json')
+      res.write(JSON.stringify({
+        serversAvailable: [
+          // Send only one server name.
+          { serverName: 'Development 1', location: 'http://localhost:4000' }
+        ]
+      }))
+      res.end()
+      break
+    }
+    default: {
+      res.statusCode = 400
+      next(new Error('Unrecognized for parameter!'))
+      break
+    }
+  }
 })
 app.use((req, res) => {
   finalHandler(req, res)()
+})
+app.use((err, req, res, next) => {
+  finalHandler(req, res)(err)
 })
 
 server.on('error', err => {
