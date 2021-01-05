27699bfdf135556dc30d4191eab82f74c7445614
Take-Some-Bytes <57299445+Take-Some-Bytes@users.noreply.github.com>
2021-01-04 21:01:56 -0800

    Drastically refactored code

--- client/js/apps/lobby-app.js
+++ client/js/apps/lobby-app.js
@@ -3,7 +3,10 @@
  * @fileoverview Main lobby app class.
  */
 
-import Dialog from '../ui/dialog.js'
+import Fetcher from '../helpers/fetcher.js'
+import PlayDialog from '../components/play-dialog.js'
+import SettingsDialog from '../components/settings-dialog.js'
+
 import * as constantUtils from '../constants.js'
 
 /**
@@ -15,14 +18,10 @@ export default class LobbyApp {
    */
   constructor () {
     this.constants = constantUtils.getConstants()
+    this.fetcher = new Fetcher({
+      constants: this.constants
+    })
 
-    /**
-     * @type {Object<string, Dialog>}
-     */
-    this.dialogs = {
-      Play: null,
-      Settings: null
-    }
     this.confs = {
       baseDialogConf: {
         renderTarget: document.querySelector('body'),
@@ -33,69 +32,26 @@ export default class LobbyApp {
         draggable: true,
         'min-width': 240,
         'min-height': 240
-      },
-      buttons: {
-        Play: ['Next', 'Play']
       }
     }
-    this.playDialogState = {
-      onSlide: 0
+    this.components = {
+      playDialog: null,
+      settingsDialog: null
     }
   }
 
   /**
-   * TODO: Fill in the next three functions.
-   * Currently, the following three functions are just no-ops-and that's okay...
-   * for now. They will have to something in the next version.
-   * (12/22/2020) Take-Some-Bytes */
-
-  /**
-   * Handles when the player presses the next button.
-   * @param {Event} e The event.
-   * @private
+   * Creates the required components for this lobby application.
    */
-  _onPlayNext (e) {}
-
-  /**
-   * Handles when the player presses the Play button in the play dialog.
-   * @param {Event} e The event.
-   */
-  _onPlay (e) {}
-
-  /**
-   * Handles when the game settings gets saved.
-   * @param {Event} e The event.
-   */
-  _onSettingsSave (e) {}
-
-  /**
-   * Creates the required dialogs.
-   */
-  createDialogs () {
-    Object.keys(this.dialogs).forEach(name => {
-      const dialog = new Dialog({
-        width: this.constants.VIEWPORT_WIDTH,
-        height: this.constants.VIEWPORT_HEIGHT
-      }, name)
-
-      Object.keys(this.confs.baseDialogConf).forEach(confKey => {
-        dialog.set(confKey, this.confs.baseDialogConf[confKey])
-      })
-
-      dialog
-        .set('width', Math.round(this.constants.VIEWPORT_WIDTH / 3))
-        .set('height', Math.round(this.constants.VIEWPORT_HEIGHT * 10 / 1.5 / 10))
-        .set(
-          'x',
-          Math.round(this.constants.VIEWPORT_WIDTH / 2) - dialog.get('width') / 2
-        ).set(
-          'y',
-          Math.round(this.constants.VIEWPORT_HEIGHT / 2) - dialog.get('height') / 2
-        ).set(
-          'title', name
-        )
-
-      this.dialogs[name] = dialog
+  createComponents () {
+    this.components.playDialog = new PlayDialog({
+      constants: this.constants,
+      dialogConf: this.confs.baseDialogConf,
+      fetcher: this.fetcher
+    })
+    this.components.settingsDialog = new SettingsDialog({
+      constants: this.constants,
+      dialogConf: this.confs.baseDialogConf
     })
   }
 
@@ -113,69 +69,21 @@ export default class LobbyApp {
   }
 
   /**
-   * Sets the current dialog buttons.
-   */
-  setDialogButtons () {
-    Object.keys(this.dialogs).forEach(name => {
-      const dialog = this.dialogs[name]
-
-      dialog.setButton(
-        'Cancel', e => {
-          e.preventDefault()
-          dialog.set('show', false)
-        }
-      )
-      if (name === 'Play') {
-        // We're on the Play dialog–the buttons matter.
-        dialog.setButton(
-          this.confs.buttons.Play[this.playDialogState.onSlide],
-          this.playDialogState.onSlide === 0
-            ? this._onPlayNext.bind(this)
-            : this._onPlay.bind(this)
-        )
-      } else {
-        // Settings dialog only has two buttons, and they both stay the same.
-        dialog.setButton(
-          'Save', this._onSettingsSave.bind(this)
-        )
-      }
-    })
-  }
-
-  /**
-   * Updates the dialog's dimensions.
+   * Updates both dialogs' dimensions.
    */
   updateDialogDimensions () {
-    Object.keys(this.dialogs).forEach(name => {
-      const dialog = this.dialogs[name]
-
-      dialog
-        .set('width', Math.round(this.constants.VIEWPORT_WIDTH / 3))
-        .set('height', Math.round(this.constants.VIEWPORT_HEIGHT * 10 / 1.5 / 10))
-        .set('x', Math.round(this.constants.VIEWPORT_WIDTH / 2) - dialog.get('width') / 2)
-        .set('y', Math.round(this.constants.VIEWPORT_HEIGHT / 2) - dialog.get('height') / 2)
-    })
+    this.components.settingsDialog.updateDimensions()
+    this.components.playDialog.updateDimensions()
   }
 
   /**
    * Registers the event listeners for the lobby application.
    */
   registerEventListeners () {
-    ;['Settings', 'Play'].forEach(name => {
-      const button = document.querySelector(
-        `#${name.toLowerCase()}-href`
-      )
-      const dialog = this.dialogs[name]
-
-      button.addEventListener('click', e => {
-        e.preventDefault()
-
-        dialog.set('show', true)
-      })
-    })
-
     window.addEventListener('resize', () => {
-      this.constants = constantUtils.getConstants()
+      this.constants = Object.assign(
+        this.constants, constantUtils.getConstants()
+      )
       this.updateDialogDimensions()
     })
   }
@@ -183,10 +91,11 @@ export default class LobbyApp {
   /**
    * Runs the lobby app.
    */
-  run () {
+  async run () {
     this.displayVersion()
-    this.createDialogs()
-    this.setDialogButtons()
+    this.createComponents()
     this.registerEventListeners()
+    await this.components.playDialog.init()
+    await this.components.settingsDialog.init()
   }
 }
