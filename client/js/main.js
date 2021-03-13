cdd2dda21ed903d2f1f909967e46aa3e95c9a310
Take-Some-Bytes <57299445+Take-Some-Bytes@users.noreply.github.com>
2021-03-12 20:57:35 -0800

    Added my name

--- client/js/main.js
+++ client/js/main.js
@@ -8,7 +8,7 @@
  * The only reason I used dynamic import here is so that we could
  * dynamically load the right app class, based on what page
  * this file was loaded from.
- * (12/22/2020) */
+ * (12/22/2020) Take-Some-Bytes */
 ;(async () => {
   const App = (await import('./apps/lobby-app.js')).default
   new App().run()
