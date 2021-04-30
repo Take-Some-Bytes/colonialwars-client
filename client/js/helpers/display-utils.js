e2a83214e139acfa91d9a7b0ae1671d4d4b2b2fc
Take-Some-Bytes <horton0712@gmail.com>
2021-04-29 21:31:09 -0700

    Added new display and image helpers

---
+++ client/js/helpers/display-utils.js
@@ -0,0 +1,117 @@
+/* eslint-env browser */
+/**
+ * @fileoverview Utilities for displaying stuff to the user.
+ */
+
+import EventEmitter from '../event-emitter.js'
+
+import * as validator from '../validation/validator.js'
+
+/**
+ * @typedef {import('../validation/validator').ValidationError} ValidationError
+ *
+ * @typedef {Object} ErrorDisplayerOptions
+ * @prop {Element} elem
+ * @prop {Array<string>} [classes]
+ */
+
+/**
+ * ErrorDisplayer class.
+ */
+export class ErrorDisplayer {
+  /**
+   * Constructor for an ErrorDisplayer class. This class
+   * displays validation errors.
+   * @param {ErrorDisplayerOptions} opts Options.
+   */
+  constructor (opts) {
+    const { elem, classes } = opts
+
+    this.classes = classes || []
+
+    this._errorMsg = null
+
+    this.elem = elem
+  }
+
+  /**
+   * Displays the specified error.
+   * @param {Error} error The error to display.
+   * @param {boolean} isValidationErr Whether the error is a validation error.
+   */
+  display (error, isValidationErr) {
+    this.classes.forEach(cls => {
+      this.elem.classList.add(cls)
+    })
+
+    if (isValidationErr && error instanceof validator.ValidationError) {
+      this._errorMsg = document.createTextNode(
+        `${error.message} To fix this issue, ${error.toFix.toLowerCase()}`
+      )
+    } else {
+      this._errorMsg = document.createTextNode(
+        error.message
+      )
+    }
+    this.elem.appendChild(this._errorMsg)
+  }
+
+  /**
+   * Undisplays the displayed error.
+   */
+  undisplay () {
+    this.classes.forEach(cls => {
+      this.elem.classList.remove(cls)
+    })
+    console.log(this._errorMsg)
+    if (this._errorMsg instanceof Text && this.elem.contains(this._errorMsg)) {
+      this.elem.removeChild(this._errorMsg)
+      this._errorMsg = null
+      console.log(this._errorMsg)
+    }
+  }
+
+  /**
+   * Sets the element which this ErrorDisplayer is attached to.
+   * @param {Element} newElem The new element to display errors on.
+   */
+  setElem (newElem) {
+    this.elem = newElem
+  }
+}
+
+/**
+ * ViewportDimensions class to store the current viewport dimensions.
+ * @extends EventEmitter
+ */
+export class ViewportDimensions extends EventEmitter {
+  /**
+   * Constructor for a ViewportDimensions class.
+   */
+  constructor () {
+    super()
+    this.width = 0
+    this.height = 0
+
+    // Get initial viewport dimensions.
+    this.update()
+  }
+
+  /**
+   * Updates the stored viewport dimensions.
+   */
+  update () {
+    if (window.innerWidth !== undefined) {
+      this.width = window.innerWidth
+    } else {
+      this.width = document.documentElement.clientWidth
+    }
+
+    if (window.innerHeight !== undefined) {
+      this.height = window.innerHeight
+    }
+    this.height = document.documentElement.clientHeight
+
+    this.emit('update')
+  }
+}
---
+++ client/js/helpers/image-helpers.js
@@ -0,0 +1,92 @@
+/* eslint-env browser */
+/**
+ * @fileoverview Helper functions and classes for messing with images.
+ */
+
+import debugFactory from '../debug.js'
+
+const debug = debugFactory('colonialwars:image-helpers')
+
+/**
+ * @typedef {Object} ImageLoaderOptions
+ * @prop {string} baseURL The URL to load images relative from.
+ */
+
+/**
+ * Converts a Blob object to a HTMLImageElement.
+ * @param {Blob} blob The Blob object ot convert.
+ * @returns {Promise<HTMLImageElement>}
+ */
+export function blobToImage (blob) {
+  return new Promise((resolve, reject) => {
+    if (!(blob instanceof Blob)) {
+      reject(new TypeError('The blob parameter must be a valid blob!'))
+    }
+    const objURL = URL.createObjectURL(blob)
+    const img = new Image()
+    img.addEventListener('load', () => {
+      URL.revokeObjectURL(objURL)
+      resolve(img)
+    })
+    img.addEventListener('error', e => {
+      reject(e)
+    })
+    img.src = objURL
+  })
+}
+
+/**
+ * ImageLoader class.
+ */
+export class ImageLoader {
+  /**
+   * Constructor for an image loader class. The image loader class loads
+   * images, storing them in an internal cache.
+   * @param {ImageLoaderOptions} opts Options.
+   */
+  constructor (opts) {
+    const { baseURL } = opts
+
+    this.baseURL = baseURL
+
+    /**
+     * The internal cache of images that has been loaded.
+     * @type {Map<string, InstanceType<Image>>}
+     */
+    this._imgCache = new Map()
+  }
+
+  /**
+   * Loads an image. Uses a cached image if it exists.
+   * @param {string} path The path of the image, relative to this ImageLoader's
+   * baseURL property.
+   * @param {boolean} [force=false] Whether to force the load. If true, this method
+   * will not try to find the image in this ImageLoader's image cache. Default false.
+   * @returns {Promise<HTMLImageElement>}
+   */
+  loadImg (path, force = false) {
+    debug('Fetching image from path %s', path)
+    if (this._imgCache.has(path) && !force) {
+      debug('Image fetched from cache.')
+      return Promise.resolve(this._imgCache.get(path))
+    }
+
+    return new Promise((resolve, reject) => {
+      const img = new Image()
+      const imgPath = new URL(path, this.baseURL).pathname
+      debug(this.baseURL)
+      debug('Absolute image path: %s', imgPath)
+
+      img.src = imgPath
+
+      img.addEventListener('error', e => {
+        e.preventDefault()
+        reject(new Error(e.error))
+      })
+      img.addEventListener('load', () => {
+        this._imgCache.set(path, img)
+        resolve(img)
+      })
+    })
+  }
+}
