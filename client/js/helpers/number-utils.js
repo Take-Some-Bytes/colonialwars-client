fcf9a1073c924eb380824b13a20e4f1f01f4c074
Take-Some-Bytes <horton0712@gmail.com>
2021-04-29 21:32:23 -0700

    Added and renamed number utilities

--- client/js/helpers/number-utils.js
+++ client/js/helpers/number-utils.js
@@ -4,14 +4,52 @@
  */
 
 /**
- * Binds a number to the given minimum and maximum, inclusive of both
- * binds. This function will still work if min and max are switched.
- * @param {number} val The value to check.
- * @param {number} min The minimum number to bound to.
- * @param {number} max The maximum number to bound to.
+ * Bounds a number to a minimum and maximum. This function
+ * will still work if min and max are switched (i.e. min is larger than max).
+ * @param {number} val The value to bound.
+ * @param {number} min The minimum bound.
+ * @param {number} max The maximum bound.
  * @returns {number}
  */
-export function bind (val, min, max) {
-  if (min > max) { return Math.min(Math.max(val, max), min) }
-  return Math.min(Math.max(val, min), max)
+export function bound (val, min, max) {
+  if (min > max) return Math.min(min, Math.max(max, val))
+  return Math.min(max, Math.max(min, val))
+}
+/**
+ * Returns a boolean representing whether the given value is between the minimum
+ * and maximum, inclusive of both bounds. This function will still work if
+ * ``min`` and ``max`` are switched.
+ * @param {number} val The value to check.
+ * @param {number} min The minimum bound.
+ * @param {number} max The maximum bound.
+ */
+export function inBound (val, min, max) {
+  if (min > max) { return val >= max && val <= min }
+  return val >= min && val <= max
+}
+/**
+ * Finds all the divisors (i.e. factors) of num.
+ * @param {number} num The number to find all the divisors of.
+ * @returns {Array<number>}
+ */
+export function findAllDivisors (num) {
+  let divisors = []
+
+  for (let i = 0, stop = Math.sqrt(num); i < stop; i++) {
+    if (num % i === 0) {
+      if (i === num / i) {
+        divisors.push(i)
+      } else {
+        divisors.push(i, num / i)
+      }
+    }
+  }
+
+  // Sort the divisors from least to greatest.
+  divisors = divisors
+    // All values sorted must be a number, not NaN and not Infinity.
+    .filter(val => typeof val === 'number' && !isNaN(val) && isFinite(val))
+    .sort((a, b) => a - b)
+
+  return divisors
 }
